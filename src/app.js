ef9ed1daf224a6a0fd756b2c23534632594fc4c8
Rongjian Zhang <pd4d10@gmail.com>
2018-08-26 22:41:36 +0800

    Basic implementation

--- src/app.js
+++ src/app.js
@@ -1,18 +1,78 @@
 import React, { Component } from 'react'
+import html2canvas from 'html2canvas'
+import { Cascader, Button } from 'antd'
+import { saveAs } from 'file-saver'
+import { devices } from './devices'
 
-class App extends Component {
+export default class App extends Component {
+  state = {
+    container: null,
+    screenshot: null,
+  }
+
+  $container = null
+
   render() {
+    const { container, screenshot, top, left } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Cascader
+          options={devices}
+          onChange={(value, options) => {
+            const selected = options[options.length - 1]
+            this.setState({
+              container: selected.image,
+              top: selected.top,
+              left: selected.left,
+            })
+          }}
+          placeholder="Please select device"
+        />
+        <input
+          type="file"
+          onChange={e => {
+            // https://codepen.io/hartzis/pen/VvNGZP
+            const reader = new FileReader()
+            const file = e.target.files[0]
+
+            reader.onloadend = () => {
+              this.setState({
+                screenshot: reader.result,
+              })
+            }
+
+            reader.readAsDataURL(file)
+          }}
+        />
+        <Button
+          onClick={async () => {
+            // e.preventDefault()
+            const canvas = await html2canvas(this.$container)
+            canvas.toBlob(blob => {
+              saveAs(blob, 'test.png')
+            })
+          }}
+        >
+          Download
+        </Button>
+        <div
+          ref={element => {
+            this.$container = element
+          }}
+        >
+          {screenshot && (
+            <img
+              src={screenshot}
+              style={{
+                position: 'absolute',
+                top,
+                left,
+              }}
+            />
+          )}
+          {container && <img src={container} style={{}} />}
+        </div>
       </div>
     )
   }
 }
-
-export default App
---
+++ src/devices.js
@@ -0,0 +1,17 @@
+export const devices = [
+  {
+    value: 'Phones',
+    label: 'Phones',
+    children: [
+      {
+        value: 'Apple iPhone X',
+        label: 'Apple iPhone X',
+        image: require('./images/Phones/Apple iPhone X/Device/Apple iPhone X Silver.png'),
+        width: 2000,
+        height: 1000,
+        left: 140,
+        top: 212,
+      },
+    ],
+  },
+]
